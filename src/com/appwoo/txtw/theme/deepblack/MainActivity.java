4041951493bbe9877e2dbd6e89473e768aeff8f0
yanqiang <yan--qiang@sohu.com>
2017-03-03 19:49:26 +0800

    button

--- src/com/appwoo/txtw/theme/deepblack/MainActivity.java
+++ src/com/appwoo/txtw/theme/deepblack/MainActivity.java
@@ -2,33 +2,29 @@ package com.appwoo.txtw.theme.deepblack;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+	
+	private EditText m_editText_Input;
+	private Button m_button_Run;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+		
+		m_editText_Input = (EditText)findViewById(R.id.editText_Input);
+		m_button_Run = (Button)findViewById(R.id.button_Run);
+		
+		m_button_Run.setOnClickListener(new OnClickListener() {			
+			@Override
+		    public void onClick(View arg0) {
+				m_editText_Input.setText("");    
+		    }
+		   });
 	}
 }
