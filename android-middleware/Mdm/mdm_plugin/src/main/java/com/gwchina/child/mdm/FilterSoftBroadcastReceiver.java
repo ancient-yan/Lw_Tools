39788a74f65f2521450633fb3544f422903504a5
yanqiang <yan--qiang@sohu.com>
2021-07-12 17:07:17 +0800

    sendVerifyBroadcast

--- android-middleware/Mdm/mdm_plugin/src/main/java/com/gwchina/child/mdm/FilterSoftBroadcastReceiver.java
+++ android-middleware/Mdm/mdm_plugin/src/main/java/com/gwchina/child/mdm/FilterSoftBroadcastReceiver.java
@@ -10,15 +10,22 @@ public class FilterSoftBroadcastReceiver extends BroadcastReceiver {
     private static final String TAG = "my_mdm";
     private static final String ACTION_VERIFICATION = "com.txtw.action.package_verification.result";
     private static final String NAME_ALLOW_INSTALL = "isAllowInstall";
+    private static final String EXTRA_VERIFICATION_PACKAGE_NAME = "android.content.pm.extra.VERIFICATION_PACKAGE_NAME";
+    private static final String EXTRA_APK_PATH = "apk_install_path";
 
     @Override
     public void onReceive(final Context context, final Intent intent) {
         String strAction = intent.getAction();
-        Log.e(TAG, "FilterSoftBroadcastReceiver.onReceive : " + strAction);
+        Log.i(TAG, "FilterSoftBroadcastReceiver.onReceive : " + strAction);
 
         if ("com.txtw.package.verify.receiver".equals(strAction)) {
+            final String packageName = intent.getStringExtra(EXTRA_VERIFICATION_PACKAGE_NAME);
+            final String ApkPath = intent.getStringExtra(EXTRA_APK_PATH);
             final int nVerifiedId = intent.getIntExtra(PackageManager.EXTRA_VERIFICATION_ID, -1);
-//            sendVerifyBroadcast(context, nVerifiedId, true);
+
+            Log.i(TAG, "FilterSoftBroadcastReceiver.packageName ：" + packageName + "; ApkPath : " + ApkPath);
+
+            sendVerifyBroadcast(context, nVerifiedId, true);
         }
     }
 
@@ -28,6 +35,6 @@ public class FilterSoftBroadcastReceiver extends BroadcastReceiver {
         intent.putExtra(NAME_ALLOW_INSTALL, allow);
         context.sendBroadcast(intent);
 
-        Log.e(TAG, "广播回应告知是否允许安装 ：" + verifiedId + " allow : " + allow);
+        Log.i(TAG, "广播回应告知是否允许安装 ：" + verifiedId + " allow : " + allow);
     }
 }
