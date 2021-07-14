4ca891a94083df70cf8f55fcc422b59936ef0581
yanqiang <yan--qiang@sohu.com>
2021-07-14 11:31:04 +0800

    setReset

--- android-middleware/Mdm/mdm_plugin/src/main/java/com/gwchina/child/mdm/core/glDeviceInterfaceImpl.java
+++ android-middleware/Mdm/mdm_plugin/src/main/java/com/gwchina/child/mdm/core/glDeviceInterfaceImpl.java
@@ -59,7 +59,7 @@ public class glDeviceInterfaceImpl extends AbstractDeviceInterfaceImpl {
 
         mDeviceControlManager = new lwDeviceControlManagerImpl(mpm);
         mDeviceRestrictionManager = new lwDeviceRestrictionManagerImpl(mpm);
-        mDeviceSettingsManager = new lwDeviceSettingsManagerImpl();
+        mDeviceSettingsManager = new lwDeviceSettingsManagerImpl(mpm);
         mDeviceApplicationManager = new lwDeviceApplicationManagerImpl();
         mDevicePackageManager = new lwDevicePackageManagerImpl();
 
--- android-middleware/Mdm/mdm_plugin/src/main/java/com/gwchina/child/mdm/core/lw/lwDeviceSettingsManagerImpl.java
+++ android-middleware/Mdm/mdm_plugin/src/main/java/com/gwchina/child/mdm/core/lw/lwDeviceSettingsManagerImpl.java
@@ -1,33 +1,28 @@
 package com.gwchina.child.mdm.core.lw;
 
+import android.app.mia.MiaMdmPolicyManager;
 import android.util.Log;
 
-import com.gwchina.child.mdm.DeviceService;
 import com.gwchina.child.mdm.core.glDeviceSettingsManagerImpl;
 
 public class lwDeviceSettingsManagerImpl extends glDeviceSettingsManagerImpl {
+    MiaMdmPolicyManager mpm;
+
+    public lwDeviceSettingsManagerImpl(MiaMdmPolicyManager mpm) {
+        this.mpm = mpm;
+    }
+
     @Override
     public boolean setRestoreFactoryDisabled(boolean disable) {
         Log.i(TAG, "setRestoreFactoryDisabled : " + disable);
 
         try {
+            mpm.setReset(disable);
         } catch (Throwable e) {
             Log.e(TAG, "setRestoreFactoryDisabled : " + e);
         }
 
         return super.setRestoreFactoryDisabled(disable);
-    }
-
-    @Override
-    public boolean isRestoreFactoryDisabled() {
-        Log.i(TAG, "isRestoreFactoryDisabled");
-
-        try {
-        } catch (Throwable e) {
-            Log.e(TAG, "isRestoreFactoryDisabled : " + e);
-        }
-
-        return super.isRestoreFactoryDisabled();
     }
 
     @Override
