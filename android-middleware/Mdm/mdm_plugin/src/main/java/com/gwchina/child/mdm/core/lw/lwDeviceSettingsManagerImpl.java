50b2258f9b2eecb36f7c05a1e91ad87c43778a3f
yanqiang <yan--qiang@sohu.com>
2023-01-13 11:17:22 +0800

    getComponentEnabledSetting

--- android-middleware/Mdm/mdm_plugin/src/main/java/com/gwchina/child/mdm/core/lw/lwDeviceSettingsManagerImpl.java
+++ android-middleware/Mdm/mdm_plugin/src/main/java/com/gwchina/child/mdm/core/lw/lwDeviceSettingsManagerImpl.java
@@ -3,6 +3,7 @@ package com.gwchina.child.mdm.core.lw;
 import android.app.mia.MiaMdmPolicyManager;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.pm.PackageManager;
 
 import com.gwchina.child.mdm.core.glDeviceSettingsManagerImpl;
 
@@ -15,16 +16,18 @@ public class lwDeviceSettingsManagerImpl extends glDeviceSettingsManagerImpl {
         this.context = context;
     }
 
+    private static final ComponentName componentNameRestoreFactory
+            = new ComponentName("com.android.settings", "com.android.settings.Settings$PrivacySettingsActivity");
+
     @Override
     public boolean setRestoreFactoryDisabled(boolean disable) {
-        lwDeviceApplicationManagerImpl.setComponentEnabled(context,
-                new ComponentName("com.android.settings", "com.android.settings.Settings$PrivacySettingsActivity"),
-                !disable);
+        lwDeviceApplicationManagerImpl.setComponentEnabled(context, componentNameRestoreFactory, !disable);
         return true;
     }
 
     @Override
     public boolean isRestoreFactoryDisabled() {
-        return false;
+        return PackageManager.COMPONENT_ENABLED_STATE_DISABLED
+                == context.getPackageManager().getComponentEnabledSetting(componentNameRestoreFactory);
     }
 }
